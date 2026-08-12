b6128ababd8c4f5d188c8d963cb873c7096ffa4c
Uplinkpro <uplinkpro@outlook.com>
2026-08-11 21:59:40 -0500

    Rotate updater release signing key

--- Source/CartLaunchCompanion.Core/Updating/OfficialUpdateTrust.cs
+++ Source/CartLaunchCompanion.Core/Updating/OfficialUpdateTrust.cs
@@ -3,11 +3,13 @@ namespace CartLaunchCompanion.Core.Updating;
 public static class OfficialUpdateTrust
 {
     public const string Repository = "Uplinkpro/CartLaunchCompanion";
-    public const string KeyId = "uplinkpro-release-1";
+    public const string KeyId = "uplinkpro-release-2";
     public const string PublicKeyPem = """
         -----BEGIN PUBLIC KEY-----
-        MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEwSnjXV9pP/RuRH98cJXSq8Lw6332
-        wssHkj7Ar7CRdv4eXonx06il4+hqLG19fe0R0Qz5XfuYZqWR5/J4qcTR6A==
+        MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQAob9spemtIpwj6clGA1MJ47b0YSRI
+        LKvjffK6ggFpOxos9iP5VzQNSpykv41f5nm+B3fXkPaq0WOViBQMnrAidPoBE+3X
+        2hDLiqQdtVn0KbqNQL/xqbf4ThkW+r/9g6u6mI80Jz4clFMfvVPdTMN3wUweefzm
+        yiBAEdj/zODhs+aG/BQ=
         -----END PUBLIC KEY-----
         """;
 }
